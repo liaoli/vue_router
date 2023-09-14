6dc14770aa31a5f435b5c8deee5b6eae88166da8
liaoli <277218962@qq.com>
2023-09-14 17:18:39 +0800

    404 页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Home from '@/views/Home'
 import Search from '@/views/Search'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import NotFound from '@/views/NotFound'
 Vue.use(VueRouter) // VueRouter插件初始化
 
 // 创建了一个路由对象
@@ -9,6 +10,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/home' },
     { path: '/home', component: Home },
+    { path: '*', component: NotFound },
     { path: '/search/:words?', component: Search }
   ]
 })
