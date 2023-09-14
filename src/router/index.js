ca387f707f0526c73f1a7a8e95529db85e938cfd
liaoli <277218962@qq.com>
2023-09-14 14:51:14 +0800

    路由模块拆分

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import Find from '../views/Find'
+import My from '../views/My'
+import Friend from '../views/Friend'
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes:[
+        {path:'/find',component:Find},
+        {path:'/my',component:My},
+        {path:'/friend',component:Friend},
+    ]
+})
+
+export default router
